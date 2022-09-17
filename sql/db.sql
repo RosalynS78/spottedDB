62822267d66c030662a0621f28b013463d423527
RosalynS78 <rrcervania78@gmail.com>
2022-09-17 02:14:06 -0500

    error

--- sql/db.sql
+++ sql/db.sql
@@ -1,5 +1,5 @@
--- registers/creates a new user 
+
 "insert into users(username, email, passwordHash) values (?, ?, ?)"
 
--- logs in a new user
+
 "select id, username, passwordHash from users where email = ?"
